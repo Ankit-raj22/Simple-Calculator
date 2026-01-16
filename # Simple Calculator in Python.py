ee23d76d8b777446445963582763bd9dfb6fc151
Ankit Raj <arankitraj45@gmail.com>
2026-01-16 23:31:39 +0530

    Add files via upload

--- # Simple Calculator in Python.py
+++ # Simple Calculator in Python.py
@@ -26,7 +26,7 @@ choice = input("Enter choice (1/2/3/4): ")
 
 num1 = float(input("Enter first number: "))
 num2 = float(input("Enter second number: "))
-
+ca
 if choice == '1':
     print("Result:", add(num1, num2))
 elif choice == '2':
